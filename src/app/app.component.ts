0a53f33935a67425a9043c31b40a37cde0cfbcfc
raphael cardoso <raphael.cardoso.f@gmail.com>
2019-03-17 05:20:35 -0400

    Card created and flipped.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +7,25 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  private res: any; 
+  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
   title = 'occam-sci-card';
   readonly url = 'https://occam-dev.cs.pitt.edu/QmcHkCwYQsLVdj1jZYKHKc2rfLGnaRxwcRwCMyoqU3L9dr/5drdakP3p7FoWiSNEQQMTY6s65Zuxa/0/output?link=73&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWFkT25seSI6IlFtY2F0Q0p4MnFTeEhockJWdVhySkt2ODg0VGpwNTVGMjhEcHFleFNBaG82enUifQ.R1EV7GdJqc-JEkOE7EAPCwLw1SJSqfBHG0G3T1J7vyc';
   constructor(private httpClient: HttpClient) { }
   ngOnInit() {
-    // this.httpClient.get(this.url).subscribe(res => {
-    //   console.log(res);
-    // });
-
-
+    this.httpClient.get(this.url).subscribe(res => {
+      this.res = res;
+      console.log(this.res);
+    });
   }
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule, MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartsModule } from 'ng2-charts';
+import {OccamRequesterService} from '../services/occam-requester.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,13 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    ChartsModule
   ],
-  providers: [],
+  providers: [OccamRequesterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
