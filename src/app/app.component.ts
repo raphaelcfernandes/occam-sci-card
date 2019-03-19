25a3af97f665aac530d26c14b02b6b55704c7fae
raphael cardoso <raphael.cardoso.f@gmail.com>
2019-03-19 00:08:47 -0400

    Understanding occam url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,25 +21,20 @@ import {trigger, state, style, transition, animate} from '@angular/animations';
 })
 
 export class AppComponent implements OnInit {
-  public barChartOptions = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
+
   private fileList: any = [];
   private invalidFiles: any = [];
   private flip = 'inactive';
   private res: any;
-  private btnStyle = 'flip-card-inner';
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType = 'bar';
-  public barChartLegend = true;
-  public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  ];
+
   title = 'occam-sci-card';
-  readonly url = 'https://occam-dev.cs.pitt.edu/QmcHkCwYQsLVdj1jZYKHKc2rfLGnaRxwcRwCMyoqU3L9dr/5drdakP3p7FoWiSNEQQMTY6s65Zuxa/0/output?link=73&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWFkT25seSI6IlFtY2F0Q0p4MnFTeEhockJWdVhySkt2ODg0VGpwNTVGMjhEcHFleFNBaG82enUifQ.R1EV7GdJqc-JEkOE7EAPCwLw1SJSqfBHG0G3T1J7vyc';
+  //This is the link of the experiment the user will give as readonly
+  readonly url = 'https://occam-dev.cs.pitt.edu/QmcHkCwYQsLVdj1jZYKHKc2rfLGnaRxwcRwCMyoqU3L9dr/' +
+                  '5drdakP3p7FoWiSNEQQMTY6s65Zuxa/' +
+                  '0?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWFkT25seSI6IlFtY2F0Q0p4MnFTeEhockJWd' +
+                  'VhySkt2ODg0VGpwNTVGMjhEcHFleFNBaG82enUifQ.R1EV7GdJqc-JEkOE7EAPCwLw1SJSqfBHG0G3T1J7vyc&link=73';
   constructor(private httpClient: HttpClient) { }
+  
   ngOnInit() {
     this.httpClient.get(this.url).subscribe(res => {
       this.res = res;
